a806a1a2cd920eb7a1ece0d8a27de40b08842df3
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2024-05-20 18:14:09 +0200

    add navigtion dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
