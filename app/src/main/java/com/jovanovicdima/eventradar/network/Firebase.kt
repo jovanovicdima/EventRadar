072587897b38ac6b5e134bb4c369a45fd7123d61
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2024-05-23 14:34:26 +0200

    add profile picture to the registration process

--- app/src/main/java/com/jovanovicdima/eventradar/network/Firebase.kt
+++ app/src/main/java/com/jovanovicdima/eventradar/network/Firebase.kt
@@ -1,10 +1,16 @@
 package com.jovanovicdima.eventradar.network
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 object Firebase {
 
@@ -14,6 +20,7 @@ object Firebase {
         username: String,
         fullName: String,
         phoneNumber: String,
+        image: ImageBitmap,
         successCallback: () -> Unit,
         failureCallback: () -> Unit
     ) {
@@ -22,7 +29,7 @@ object Firebase {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnSuccessListener {
                 Log.d("Register", "SUCCESSFUL")
-                bindUsernameToEmail(email, username, fullName, phoneNumber, successCallback, failureCallback)
+                bindUsernameToEmail(email, username, fullName, phoneNumber, image, successCallback, failureCallback)
             }
             .addOnFailureListener { exception ->
                 Log.e("Auth", exception.toString())
@@ -36,6 +43,7 @@ object Firebase {
         username: String,
         fullName: String,
         phoneNumber: String,
+        image: ImageBitmap,
         successCallback: () -> Unit,
         failureCallback: () -> Unit
     ) {
@@ -47,7 +55,7 @@ object Firebase {
             )
         ).addOnSuccessListener {
             Log.d("UsernameToPassword", "SUCCESSFUL")
-            writeAdditionalDataOnRegistration(email, username, fullName, phoneNumber, successCallback, failureCallback)
+            uploadImageToFirestore(email, username, fullName, phoneNumber, image, successCallback, failureCallback)
         }.addOnFailureListener {// Revert registration process if anything fails
             // Delete account if bind fails
             auth.currentUser!!.delete()
@@ -55,11 +63,46 @@ object Firebase {
         }
     }
 
+    private fun uploadImageToFirestore(
+        email: String,
+        username: String,
+        fullName: String,
+        phoneNumber: String,
+        image: ImageBitmap,
+        successCallback: () -> Unit,
+        failureCallback: () -> Unit
+    )  {
+        val ref = FirebaseStorage.getInstance().reference.child("images/${FirebaseAuth.getInstance().currentUser!!.uid}.jpg")
+
+        val stream = ByteArrayOutputStream()
+        image.asAndroidBitmap().compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+        ref.putBytes(stream.toByteArray())
+            .addOnSuccessListener {
+                ref.downloadUrl.addOnSuccessListener { uri ->
+                    val downloadUrl = uri.toString()
+                    // Handle the success (e.g., save the URL or update UI)
+                    Log.d("FirebaseStorage", "Upload successful, URL: $downloadUrl")
+                    writeAdditionalDataOnRegistration(email, username, fullName, phoneNumber, downloadUrl, successCallback, failureCallback)
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle the failure
+                Log.e("FirebaseStorage", "Upload failed", exception)
+                Firebase.firestore.collection("usernames").document(username).delete()
+                // Delete account
+                val auth: FirebaseAuth = FirebaseAuth.getInstance()
+                auth.currentUser!!.delete()
+                failureCallback()
+            }
+    }
+
     private fun writeAdditionalDataOnRegistration(
         email: String,
         username: String,
         fullName: String,
         phoneNumber: String,
+        profilePictureURL: String,
         successCallback: () -> Unit,
         failureCallback: () -> Unit
     ) {
@@ -70,12 +113,15 @@ object Firebase {
                 "username" to username,
                 "email" to email,
                 "fullName" to fullName,
-                "phoneNumber" to phoneNumber
+                "phoneNumber" to phoneNumber,
+                "profilePictureURL" to profilePictureURL
             )
         ).addOnSuccessListener {
             Log.d("writeAdditionalData", "SUCCESSFUL")
             successCallback()
         }.addOnFailureListener { // Revert registration process if anything fails
+            // Remove profile picture from firestore
+            FirebaseStorage.getInstance().reference.child("images/${FirebaseAuth.getInstance().currentUser!!.uid}.jpg").delete()
             // Unbind username from email
             Firebase.firestore.collection("usernames").document(username).delete()
             // Delete account
