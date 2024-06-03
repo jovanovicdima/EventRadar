7922fc58e1cbff50349d62aa196a29313dc2bed2
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2024-06-03 16:10:45 +0200

    add location service

---
+++ app/src/main/java/com/jovanovicdima/eventradar/services/LocationService.kt
@@ -0,0 +1,94 @@
+package com.jovanovicdima.eventradar.services
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.LocationServices
+import com.jovanovicdima.eventradar.R
+import com.jovanovicdima.eventradar.data.DefaultLocationClient
+import com.jovanovicdima.eventradar.data.LocationClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class LocationService: Service() {
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private lateinit var locationClient: LocationClient
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val channel = NotificationChannel(
+            "locationservicechannel",
+            "Location",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        locationClient = DefaultLocationClient(
+            applicationContext,
+            LocationServices.getFusedLocationProviderClient(applicationContext)
+        )
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        start()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+        val notification = NotificationCompat.Builder(this, "locationservicechannel")
+            .setContentTitle("Tracking location...")
+            .setContentText("Location: unknown")
+            .setSmallIcon(R.drawable.logo)
+            .setOngoing(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        locationClient
+            .getLocationUpdates(10000L)
+            .onEach { location ->
+                Log.d("SERVICE", location.toString())
+                val lat = location.latitude.toString()
+                val long = location.longitude.toString()
+                val updatedNotification = notification.setContentText(
+                    "Location: ($lat, $long)"
+                )
+                notificationManager.notify(1, updatedNotification.build())
+            }
+            .launchIn(serviceScope)
+
+        Log.d("LOCATION SERVICE", "Service started.")
+        startForeground(1, notification.build())
+    }
+
+    private fun stop() {
+        Log.d("LOCATION SERVICE", "Service stopped.")
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
+
+    override fun stopService(name: Intent?): Boolean {
+        stop()
+        return super.stopService(name)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+}
