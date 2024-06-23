166f249dcff51c8f96f5e5018bef0d3ab153708d
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2024-06-23 17:22:46 +0200

    add pin class

---
+++ app/src/main/java/com/jovanovicdima/eventradar/data/Pin.kt
@@ -0,0 +1,10 @@
+package com.jovanovicdima.eventradar.data
+
+class Pin {
+    var user: String = ""
+    var title: String = ""
+    var latitude: Double? = null
+    var longitude: Double? = null
+    var description: String = ""
+    var preview: String = ""
+}
