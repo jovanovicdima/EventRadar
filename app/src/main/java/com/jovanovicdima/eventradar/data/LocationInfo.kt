0248e62b5745161109524e99d97472475fb58f6b
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2024-06-25 03:07:11 +0200

    update unsubscribe function

---
+++ app/src/main/java/com/jovanovicdima/eventradar/data/LocationInfo.kt
@@ -0,0 +1,26 @@
+package com.jovanovicdima.eventradar.data
+
+import android.location.Location
+
+object LocationInfo {
+    var location: Location? = null
+
+    private var list: MutableList<(Location?) -> Unit> = mutableListOf()
+
+    fun Alert() {
+        for(item in list) {
+            item(location)
+        }
+    }
+
+    fun Subscribe(callback: (Location?) -> Unit) {
+        list.add(callback)
+    }
+
+    fun UnsubscribeAll() {
+        for(item in list) {
+        item(null)
+    }
+        list = mutableListOf()
+    }
+}
